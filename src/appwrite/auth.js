8694b0c8814ec2834074f82756537ca90876f22d
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-24 11:43:50 +0600

    implements constructor function

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,7 +1,15 @@
 import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
 
 export class AuthService {
   client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appWriteUrl)
+      .setProject(conf.appWriteProjectId);
+  }
 }
 
 const authService = new AuthService();
