45dd3dd01a3835a43ec93670110baf663aaec516
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-26 00:37:45 +0600

    updated file & implements provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+// import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -20,7 +23,15 @@ function App() {
       })
       .finally(() => setLoading(false));
   }, []);
-  return !loading ? <div></div> : null;
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header></Header>
+        Todo<main>{/* <Outlet></Outlet> */}</main>
+        <Footer></Footer>
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
