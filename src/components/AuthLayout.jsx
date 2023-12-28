3b7c7244123acf4a64d8ecd5c85e63af45e15991
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-29 05:34:23 +0600

    successfully completed layoutAuthentication

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-export const Protected = ({ children, authentication }) => {
+export const Protected = ({ children, authentication = true }) => {
   const navigate = useNavigate();
   const [loader, setLoader] = useState(true);
   const authStatus = useSelector((state) => state.auth.status);
