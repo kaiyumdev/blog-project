14f70a8896a9352489762e4e60edbd8aa657f856
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-29 04:26:54 +0600

    successfully completed layoutAuthentication

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export const Protected = ({ children, authentication }) => {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, authentication, navigate]);
+
+  return loader ? <h1>Loading...</h1> : { children };
+};
