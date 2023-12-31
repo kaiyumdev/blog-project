47464ec17f7d3e1c521ade2b672bb87cdfa6dc76
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-31 14:11:33 +0600

    commit RTE file successfully

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Controller } from "react-hook-form";
 import { Editor } from "@tinymce/tinymce-react";
 
+//create RTE component
 export const RTE = ({ name, control, label, defaultValue }) => {
   return (
     <div className="w-full">
