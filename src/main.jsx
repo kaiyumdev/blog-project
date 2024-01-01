6608ec647cd005beb047de3a63910a4362c9e7ca
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-01-01 20:20:14 +0600

    complete router & privateRouter successfully

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,80 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/pages/Home.jsx";
+import { AuthLayout } from "./components/AuthLayout.jsx";
+import Login from "./components/pages/Login.jsx";
+import Signup from "./components/pages/Signup.jsx";
+import AllPosts from "./components/pages/AllPosts.jsx";
+import AddPost from "./components/pages/AddPost.jsx";
+import EditPost from "./components/pages/EditPost.jsx";
+import Post from "./components/pages/Post.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AllPosts />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router}></RouterProvider>
     </Provider>
   </React.StrictMode>
 );
