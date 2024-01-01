553b8beb3e5bac3e48ef87f3ac1b817ccf6c57b7
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-01-01 20:10:01 +0600

    complete Post successfully

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+import appwriteService from "../../appwrite/config";
+import Container from "../container/Container";
+import PostCard from "../PostCard";
+
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    appwriteService.getPosts().then((posts) => {
+      if (posts) {
+        setPosts(posts);
+      }
+    });
+  }, []);
+
+  if (posts.length === 0) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <h1 className="text-2xl font-bold hover:text-gray-500">
+                Login to read posts
+              </h1>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full p-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => {
+            <div className="p-2 w-1/4">
+              <PostCard {...post}></PostCard>
+            </div>;
+          })}
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
