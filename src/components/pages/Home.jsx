187c48ed2bdffdc66d1f5e7cef9604700002f6e5
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-01-04 23:22:26 +0600

    updated & commit allPosts at page successfully

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,6 +4,7 @@ import appwriteService from "../../appwrite/config";
 import Container from "../container/Container";
 import PostCard from "../PostCard";
 
+//commit Home successfully
 const Home = () => {
   const [posts, setPosts] = useState([]);
 
