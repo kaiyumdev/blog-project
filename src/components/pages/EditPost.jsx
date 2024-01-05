d1e5e068b2a2f741f6893f2f15a137e2a7d62763
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-01-05 19:50:39 +0600

    updated editPost at page successfully

--- src/components/pages/EditPost.jsx
+++ src/components/pages/EditPost.jsx
@@ -5,7 +5,6 @@ import appwriteService from "../../appwrite/config";
 import Container from "../container/Container";
 import { PostForm } from "../post/PostForm";
 
-//create editPost
 const EditPost = () => {
   const [post, setPost] = useState();
   const { slug } = useParams();
