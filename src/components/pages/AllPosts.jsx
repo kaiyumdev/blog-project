de6feb877665f7c9d2472188f020594ccf387364
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-01-20 23:37:34 +0600

    updated AllPosts at page successfully

--- src/components/pages/AllPosts.jsx
+++ src/components/pages/AllPosts.jsx
@@ -4,7 +4,6 @@ import appwriteService from "../../appwrite/config";
 import Container from "../container/Container";
 import PostCard from "../PostCard";
 
-//create AllPosts successfully
 const AllPosts = () => {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
