f3db7a11dec4fcf3880fb23c7e60b2229bf76333
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-01-20 23:25:17 +0600

    updated addPost at page successfully

--- src/components/pages/AddPost.jsx
+++ src/components/pages/AddPost.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Container from "../container/Container";
 import { PostForm } from "../post/PostForm";
 
+//create AddPost successfully
 const AddPost = () => {
   return (
     <div>
