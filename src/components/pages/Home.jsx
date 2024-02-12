3167f1a341d53ccbc06fc5b480298194c8a2a2a9
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-02-12 10:17:31 +0600

    updated HOme at page successfully

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,6 +4,7 @@ import appwriteService from "../../appwrite/config";
 import Container from "../container/Container";
 import PostCard from "../PostCard";
 
+//updated HOme successfully
 const Home = () => {
   const [posts, setPosts] = useState([]);
 
