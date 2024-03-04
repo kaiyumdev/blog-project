b52f6a716b26b35a349624d2b4cc8626a43d60c0
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-04 23:37:07 +0600

    updated editPost at page successfully

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,6 +4,7 @@ import appwriteService from "../../appwrite/config";
 import Container from "../container/Container";
 import PostCard from "../PostCard";
 
+//updated home successfully
 const Home = () => {
   const [posts, setPosts] = useState([]);
 
