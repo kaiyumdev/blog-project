eaee006ff702f5d0ba603b3cdb10516813c4d664
kaiyumdev <afrankaiyum34@gmail.com>
2024-04-17 23:07:50 +0600

    updated AddPost at pages successfully

--- src/components/pages/AddPost.jsx
+++ src/components/pages/AddPost.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Container from "../container/Container";
 import { PostForm } from "../post/PostForm";
 
+//updated AddPost successfully
 const AddPost = () => {
   return (
     <div>
