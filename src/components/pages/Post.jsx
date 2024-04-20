bd33939214598c673a41a555e53d9037e7e85d59
kaiyumdev <afrankaiyum34@gmail.com>
2024-04-20 23:21:37 +0600

    updated Post at pages successfully

--- src/components/pages/Post.jsx
+++ src/components/pages/Post.jsx
@@ -6,6 +6,7 @@ import appwriteService from "../../appwrite/config";
 import Container from "../container/Container";
 import Button from "../Button";
 
+//update post successfully
 export default function Post() {
   const [post, setPost] = useState(null);
   const { slug } = useParams();
